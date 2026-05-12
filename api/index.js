68ab5d853252ee62d3c8ad25a4208ade77c41c0f
JCPrime01 <vanzokiller@gmail.com>
2026-05-12 19:31:42 -0300

    Add redirect handler in api/index.js

---
+++ api/index.js
@@ -0,0 +1,4 @@
+  export default function handler(req, res) {                                                                                                                                              
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/61wz68wh?shareCode=UA7YMRB134G&afp=bd1' });
+    res.end();                                                                                                                                                                             
+  }
