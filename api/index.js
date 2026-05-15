6fd97f0f7978d51d8b422f6ebe790128d0125417
JCPrime01 <272623760+JCPrime01@users.noreply.github.com>
2026-05-15 11:40:53 -0300

    chore: update shareCode to AWCSVCY8SXI

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/61wz68wh?shareCode=S6IRD3TXF7W&afp=bd1' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/61wz68wh?shareCode=AWCSVCY8SXI&afp=bd1' });
     res.end();
   }
