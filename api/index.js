d82248ef559cb689bf18e679bb00c943959c01be
JCPrime01 <vanzokiller@gmail.com>
2026-08-03 12:03:46 -0300

    chore: shareCode 6W3K3PBE42A -> VCRCQJGK3ZG (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/cj7zg19l?campaign_id=14473&shareCode=6W3K3PBE42A&afp1=bd1' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/cj7zg19l?campaign_id=14473&shareCode=VCRCQJGK3ZG&afp1=bd1' });
     res.end();
   }
